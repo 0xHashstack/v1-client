24a5f22b0bc5077b2ce3364809abf83c4e0ca5d0
zeapherine <zeapherine@gmail.com>
2022-09-23 11:26:12 +0530

    Updated the getTokens functions

--- src/blockchain/stark-constants.ts
+++ src/blockchain/stark-constants.ts
@@ -1,39 +1,40 @@
-import * as DeployDetails from "../../../zkOpen/contract_addresses.json";
-import ERC20Abi from "../../starknet-artifacts/contracts/mockups/erc20.cairo/erc20_abi.json";
-import ComptrollerAbi from "../../../zkOpen/starknet-artifacts/contracts/modules/comptroller.cairo/comptroller_abi.json";
+import * as DeployDetails from '../../../zkOpen/contract_addresses.json';
+import ERC20Abi from '../../starknet-artifacts/contracts/mockups/erc20.cairo/erc20_abi.json';
+import ComptrollerAbi from '../../../zkOpen/starknet-artifacts/contracts/modules/comptroller.cairo/comptroller_abi.json';
+
 interface ItokenAddressMap {
-  [key: string]: string | undefined;
+	[key: string]: string | undefined;
 }
 
 export const tokenAddressMap: ItokenAddressMap = {
-  BTC:
-    process.env.NODE_ENV === "development"
-      ? DeployDetails.devnet.TOKENS[0].address
-      : process.env.NEXT_PUBLIC_T_BTC,
-  USDT:
-    process.env.NODE_ENV === "development"
-      ? DeployDetails.devnet.TOKENS[2].address
-      : process.env.NEXT_PUBLIC_T_USDT,
-  USDC:
-    process.env.NODE_ENV === "development"
-      ? DeployDetails.devnet.TOKENS[1].address
-      : process.env.NEXT_PUBLIC_T_USDC,
-  BNB:
-    process.env.NODE_ENV === "development"
-      ? DeployDetails.devnet.TOKENS[3].address
-      : process.env.NEXT_PUBLIC_T_BNB,
+	BTC:
+		process.env.NODE_ENV === 'development'
+			? DeployDetails.hardhat.TOKENS[0].address
+			: process.env.NEXT_PUBLIC_T_BTC,
+	USDT:
+		process.env.NODE_ENV === 'development'
+			? DeployDetails.hardhat.TOKENS[2].address
+			: process.env.NEXT_PUBLIC_T_USDT,
+	USDC:
+		process.env.NODE_ENV === 'development'
+			? DeployDetails.hardhat.TOKENS[1].address
+			: process.env.NEXT_PUBLIC_T_USDC,
+	BNB:
+		process.env.NODE_ENV === 'development'
+			? DeployDetails.hardhat.TOKENS[3].address
+			: process.env.NEXT_PUBLIC_T_BNB,
 };
 
-export const diamondAddress: string = DeployDetails.devnet.DIAMOND_ADDRESS;
+export const diamondAddress: string = DeployDetails.hardhat.DIAMOND_ADDRESS;
 
 export const getTokenFromAddress = (address: string) => {
-  if (process.env.NODE_ENV === "development") {
-    let index = DeployDetails.devnet.TOKENS.map((item) => item.address).indexOf(
-      address
-    );
-    return DeployDetails.devnet.TOKENS[index];
-  }
-  return null;
+	if (process.env.NODE_ENV === 'development') {
+		let index = DeployDetails.hardhat.TOKENS.map(
+			(item) => item.address
+		).indexOf(address);
+		return DeployDetails.hardhat.TOKENS[index];
+	}
+	return null;
 };
 // export const getCommitmentNameFromIndex = (index: string) => {
 //   if (index == "0") {
@@ -43,27 +44,27 @@ export const getTokenFromAddress = (address: string) => {
 // };
 
 export const getTokenFromName = (name: string) => {
-  if (process.env.NODE_ENV === "development") {
-    let index = DeployDetails.devnet.TOKENS.map((item) => item.name).indexOf(
-      name
-    );
-    return DeployDetails.devnet.TOKENS[index];
-  }
-  return null;
+	if (process.env.NODE_ENV === 'development') {
+		let index = DeployDetails.hardhat.TOKENS.map((item) => item.name).indexOf(
+			name
+		);
+		return DeployDetails.hardhat.TOKENS[index];
+	}
+	return null;
 };
 
 export const getCommitmentNameFromIndex = (index: string) => {
-  if (index === "0") {
-    return "NONE";
-  } else if (index === "1") {
-    return "TWO WEEKS";
-  } else if (index === "2") {
-    return "ONE MONTH";
-  } else if (index === "3") {
-    return "THREE MONTHS";
-  }
+	if (index === '0') {
+		return 'NONE';
+	} else if (index === '1') {
+		return 'TWO WEEKS';
+	} else if (index === '2') {
+		return 'ONE MONTH';
+	} else if (index === '3') {
+		return 'THREE MONTHS';
+	}
 
-  return null;
+	return null;
 };
 // <option value={"NONE"}>None</option>
 // <option value={"TWOWEEKS"}>Two Weeks</option>
@@ -71,29 +72,29 @@ export const getCommitmentNameFromIndex = (index: string) => {
 // <option value={"THREEMONTHS"}>Three Month</option>
 
 export const getCommitmentIndexStringFromName = (name: string) => {
-  if (name === "NONE") {
-    return "0";
-  } else if (name === "TWOWEEKS") {
-    return "1";
-  } else if (name === "ONEMONTH") {
-    return "2";
-  } else if (name === "THREEMONTHS") {
-    return "3";
-  }
+	if (name === 'NONE') {
+		return '0';
+	} else if (name === 'TWOWEEKS') {
+		return '1';
+	} else if (name === 'ONEMONTH') {
+		return '2';
+	} else if (name === 'THREEMONTHS') {
+		return '3';
+	}
 };
 
 export const getCommitmentIndex = (index: string) => {
-  if (index === "0") {
-    return 0;
-  }
-  if (index === "1") {
-    return 1;
-  }
-  if (index === "2") {
-    return 2;
-  }
-  if (index === "3") {
-    return 3;
-  }
+	if (index === '0') {
+		return 0;
+	}
+	if (index === '1') {
+		return 1;
+	}
+	if (index === '2') {
+		return 2;
+	}
+	if (index === '3') {
+		return 3;
+	}
 };
 export { ERC20Abi, ComptrollerAbi };
