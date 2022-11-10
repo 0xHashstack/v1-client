e31649aab3db1786fe9a90717a86c9c4fefa8445
zeapherine <zeapherine@gmail.com>
2022-11-10 11:58:46 +0530

    rearranged min max button | added loading on min collateral amount

--- src/components/borrow.tsx
+++ src/components/borrow.tsx
@@ -45,6 +45,7 @@ let Borrow: any = ({ asset, title }: { asset: string; title: string }) => {
 	const [collateralAmount, setcollateralAmount] = useState(0);
 	const [isAllowed, setAllowed] = useState(false);
 	const [shouldApprove, setShouldApprove] = useState(false);
+	const [isLoading, setLoading] = useState(false);
 
 	const [borrowParams, setBorrowParams] = useState<IBorrowParams>({
 		loanAmount: 0,
@@ -317,6 +318,7 @@ let Borrow: any = ({ asset, title }: { asset: string; title: string }) => {
 	};
 
 	const handleMin = async () => {
+		setLoading(true);
 		const loanPrice = await getPrice(asset)
 		const collateralPrice = await getPrice(borrowParams?.collateralMarket)
 
@@ -331,6 +333,8 @@ let Borrow: any = ({ asset, title }: { asset: string; title: string }) => {
 			...borrowParams,
 			collateralAmount: minCollateral,
 		});
+		setLoading(false);
+
 	};
 
 	const handleBorrow = async (asset: string) => {
@@ -569,6 +573,27 @@ let Borrow: any = ({ asset, title }: { asset: string; title: string }) => {
 												outline
 												type='button'
 												className='btn btn-md w-xs'
+												onClick={handleMin}
+												disabled={dataBalance ? false : true}
+												style={{ background: '#2e3444', border: '#2e3444' }}
+											>
+												<span style={{ borderBottom: '2px dotted #fff' }}>
+													{ isLoading ?  
+														<Spinner style={{
+															height: '14px', 
+															width: "14px"
+															}}>
+																Loading...
+														</Spinner> : 
+														'Min'  
+													}
+												</span>
+											</Button>
+
+											<Button
+												outline
+												type='button'
+												className='btn btn-md w-xs'
 												onClick={handleMax}
 												disabled={dataBalance ? false : true}
 												style={{ background: '#2e3444', border: '#2e3444' }}
@@ -578,18 +603,7 @@ let Borrow: any = ({ asset, title }: { asset: string; title: string }) => {
 												</span>
 											</Button>
 
-											<Button
-												outline
-												type='button'
-												className='btn btn-md w-xs'
-												onClick={handleMin}
-												disabled={dataBalance ? false : true}
-												style={{ background: '#2e3444', border: '#2e3444' }}
-											>
-												<span style={{ borderBottom: '2px dotted #fff' }}>
-													Min
-												</span>
-											</Button>
+											
 											</>
 										)}
 									</InputGroup>
