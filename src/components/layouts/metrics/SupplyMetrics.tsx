dc394afe93d13530f60ab044b5976316f7071987
Sahitya77 <sahityanijhawan@gmail.com>
2023-05-17 18:43:58 +0530

    metrics update

--- src/components/layouts/metrics/SupplyMetrics.tsx
+++ src/components/layouts/metrics/SupplyMetrics.tsx
@@ -8,13 +8,13 @@ const SupplyMetrics = () => {
   return (
     <Box display="flex" gap="30px">
         <Box display="flex" flexDirection="column" gap="8px">
-            <Box display="flex" flexDirection="column" alignItems="flex-start" width="723px" height="72px" border="1px solid #2B2F35" color="#E6EDF3" padding="24px 24px 16px" fontSize="20px" fontStyle="normal" fontWeight="600" lineHeight="30px" borderRadius="6px">
+            <Box display="flex" flexDirection="column" alignItems="flex-start"  border="1px solid #2B2F35" color="#E6EDF3" padding="24px 24px 16px" fontSize="20px" fontStyle="normal" fontWeight="600" lineHeight="30px" borderRadius="6px">
                 Supply APR:
             </Box>
                 <SupplyAprChart/>
         </Box>
         <Box display="flex" flexDirection="column" gap="8px">
-            <Box display="flex" flexDirection="column" alignItems="flex-start" width="723px" height="72px" border="1px solid #2B2F35" color="#E6EDF3" padding="24px 24px 16px" fontSize="20px" fontStyle="normal" fontWeight="600" lineHeight="30px" borderRadius="6px">
+            <Box display="flex" flexDirection="column" alignItems="flex-start"  border="1px solid #2B2F35" color="#E6EDF3" padding="24px 24px 16px" fontSize="20px" fontStyle="normal" fontWeight="600" lineHeight="30px" borderRadius="6px">
                     Borrow APR:
             </Box>
                 <BorrowAprChart/>
