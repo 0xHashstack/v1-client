a8b97131aab98e385f8fed9a354255d541fc2c8e
aryan <aryan6396596910negi@gmail.com>
2023-05-31 19:41:23 +0530

    fixed build errors

--- src/components/modals/StakeUnstakeModal.tsx
+++ src/components/modals/StakeUnstakeModal.tsx
@@ -697,7 +697,7 @@ const StakeUnstakeModal = ({ buttonText, coin, ...restProps }: any) => {
                             color="#6E7681"
                           >
                             Ticking would stake the received rTokens. unchecking
-                            woudn't stake rTokens
+                            woudn&apos;t stake rTokens
                           </Text>
                         </Box>
                       )}
