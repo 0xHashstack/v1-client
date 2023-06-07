52b66661194d06f4e6598262f4c2b5ceaf708101
aryan <aryan6396596910negi@gmail.com>
2023-06-07 19:25:31 +0530

    Integrated balanceOf hook for asset wallet balance

--- src/Blockchain/hooks/Reads/useBalanceOf.tsx
+++ src/Blockchain/hooks/Reads/useBalanceOf.tsx
@@ -1,34 +1,35 @@
 import { ERC20Abi } from "../../stark-constants";
-import { useAccount,useContractRead } from "@starknet-react/core";
+import { useAccount, useContractRead } from "@starknet-react/core";
 import { useState } from "react";
 
-const useBalanceOf = ( asset: string) => {
-    const { address: accountAddress } = useAccount();
-    const {
-        data: dataBalanceOf,
+const useBalanceOf = (asset: string) => {
+  const { address: accountAddress } = useAccount();
+  const {
+    data: dataBalanceOf,
 
-        error: errorBalanceOf,
-        isIdle: isIdleBalanceOf,
+    error: errorBalanceOf,
+    isIdle: isIdleBalanceOf,
 
-        isLoading: isLoadingBalanceOf,
-        isSuccess: isSuccessBalanceOf,
-        isFetching: isFetchingBalanceOf,
-        refetch: refetchBalanceOf,
-        status: statusBalanceOf,
-    } = useContractRead({
-        address: asset,
-        abi: ERC20Abi,
-        functionName: "balanceOf",
-        args: [accountAddress],
-    })
+    isLoading: isLoadingBalanceOf,
+    isSuccess: isSuccessBalanceOf,
+    isFetching: isFetchingBalanceOf,
+    refetch: refetchBalanceOf,
+    status: statusBalanceOf,
+  } = useContractRead({
+    address: asset,
+    abi: ERC20Abi,
+    functionName: "balanceOf",
+    // args: ["0x465e14a806a77cd284873e17b9f067400d90b8169cbd8bcac6430805ca87f69"],
+    args: [accountAddress],
+  });
 
-    return {
-        dataBalanceOf,
-        errorBalanceOf,
-        isFetchingBalanceOf,
-        refetchBalanceOf,
-        statusBalanceOf,
-    };
-}
+  return {
+    dataBalanceOf,
+    errorBalanceOf,
+    isFetchingBalanceOf,
+    refetchBalanceOf,
+    statusBalanceOf,
+  };
+};
 
 export default useBalanceOf;
