c2101aa422c1a3ee9f82572f728220c52c916b8e
Mehul0011 <mehul-intern@hashstack.finance>
2023-06-09 22:26:12 +0530

    Code cleanup

--- src/Blockchain/stark-constants.ts
+++ src/Blockchain/stark-constants.ts
@@ -3,9 +3,7 @@ import ERC20Abi from "./abis/erc20_abi.json";
 import { Provider, number } from "starknet";
 import {
   UseWaitForTransactionResult,
-  UseTransactionResult,
 } from "@starknet-react/core";
-import { ItokenAddressMap, ItokenDecimalsMap } from "./interfaces/interfaces";
 
 export function processAddress(address: string) {
   return number.toHex(number.toBN(number.toFelt(address)));
@@ -73,23 +71,4 @@ export const getTokenFromAddress = (address: string) => {
   return token;
 };
 
-// export const getTokenDecimalsFromName = (name: string) =>
-
-export const getCommitmentNameFromIndexDeposit = (index: string) => {
-  if (index === "0") {
-    return "NONE";
-  } else if (index === "1") {
-    return "TWO WEEKS";
-  } else if (index === "2") {
-    return "ONE MONTH";
-  } else if (index === "3") {
-    return "THREE MONTHS";
-  }
-
-  return null;
-};
-
-export const getCommitmentIndex = (index: string) => {
-  return parseInt(index);
-};
 export { ERC20Abi, contractsEnv };
