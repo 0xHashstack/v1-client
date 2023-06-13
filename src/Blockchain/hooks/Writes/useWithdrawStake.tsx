718b3f68a98070cce76e17d62d73951f62e75d3f
aryan <aryan6396596910negi@gmail.com>
2023-06-13 19:16:17 +0530

    Updated useRepay

--- src/Blockchain/hooks/Writes/useWithdrawStake.tsx
+++ src/Blockchain/hooks/Writes/useWithdrawStake.tsx
@@ -1,58 +1,54 @@
-import { diamondAddress } from '@/Blockchain/stark-constants';
-import { tokenAddressMap } from '@/Blockchain/utils/addressServices';
-import { etherToWeiBN } from '@/Blockchain/utils/utils';
-import { useAccount, useContractWrite } from '@starknet-react/core';
-import React, { useState } from 'react'
+import { diamondAddress } from "@/Blockchain/stark-constants";
+import { tokenAddressMap } from "@/Blockchain/utils/addressServices";
+import { etherToWeiBN } from "@/Blockchain/utils/utils";
+import { useAccount, useContractWrite } from "@starknet-react/core";
+import React, { useState } from "react";
 
 const useWithdrawStake = () => {
-    const [unstakeRToken, setUnstakeRToken] = useState("")
-    const [rTokenToWithdraw, setRTokenToWithdraw] = useState(0)
-    const { address: owner } = useAccount();
+  const [unstakeRToken, setUnstakeRToken] = useState("");
+  const [rTokenToWithdraw, setRTokenToWithdraw] = useState(0);
+  const { address: owner } = useAccount();
 
+  const {
+    data: dataWithdrawStake,
+    error: errorWithdrawStake,
+    reset: resetWithdrawStake,
+    write: writeWithdrawStake,
+    writeAsync: writeAsyncWithdrawStake,
+    isError: isErrorWithdrawStake,
+    isIdle: isIdleWithdrawStake,
+    isLoading: isLoadingWithdrawStake,
+    isSuccess: isSuccessWithdrawStake,
+    status: statusWithdrawStake,
+  } = useContractWrite({
+    calls: {
+      contractAddress: diamondAddress,
+      entrypoint: "withdraw_stake",
+      calldata: [
+        tokenAddressMap[unstakeRToken],
+        etherToWeiBN(rTokenToWithdraw, unstakeRToken).toString(),
+        "0",
+        owner,
+      ],
+    },
+  });
 
-    const {
-        data: dataWithdrawStake,
-        error: errorWithdrawStake,
-        reset: resetWithdrawStake,
-        write: writeWithdrawStake,
-        writeAsync: writeAsyncWithdrawStake,
-        isError: isErrorWithdrawStake,
-        isIdle: isIdleWithdrawStake,
-        isLoading: isLoadingWithdrawStake,
-        isSuccess: isSuccessWithdrawStake,
-        status: statusWithdrawStake,
-    } = useContractWrite({
-        calls: {
-            contractAddress: diamondAddress,
-            entrypoint: "withdraw_stake",
-            calldata: [
-                tokenAddressMap[unstakeRToken],
-                etherToWeiBN(
-                    rTokenToWithdraw,
-                    unstakeRToken
-                ).toString(),
-                "0",
-                owner,
-            ]
-        }
-    })
+  return {
+    unstakeRToken,
+    setUnstakeRToken,
+    rTokenToWithdraw,
+    setRTokenToWithdraw,
+    dataWithdrawStake,
+    errorWithdrawStake,
+    resetWithdrawStake,
+    writeWithdrawStake,
+    writeAsyncWithdrawStake,
+    isErrorWithdrawStake,
+    isIdleWithdrawStake,
+    isLoadingWithdrawStake,
+    isSuccessWithdrawStake,
+    statusWithdrawStake,
+  };
+};
 
-    return {
-        unstakeRToken,
-        setUnstakeRToken,
-        rTokenToWithdraw,
-        setRTokenToWithdraw,
-        dataWithdrawStake,
-        errorWithdrawStake,
-        resetWithdrawStake,
-        writeWithdrawStake,
-        writeAsyncWithdrawStake,
-        isErrorWithdrawStake,
-        isIdleWithdrawStake,
-        isLoadingWithdrawStake,
-        isSuccessWithdrawStake,
-        statusWithdrawStake,
-    }
-}
-
-export default useWithdrawStake
+export default useWithdrawStake;
