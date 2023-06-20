1ca01ae54ab3ee94c7bca63a359d4ae7ce0f0f0c
Mehul0011 <mehul-intern@hashstack.finance>
2023-06-20 18:02:53 +0530

    fix favicon error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,8 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-
-
+        <meta httpEquiv="Cache-Control" content="no-cache, must-revalidate" />
+        <meta httpEquiv="Pragma" content="no-cache" />
+        <meta httpEquiv="Expires" content="0" />
+        <link rel="shortcut icon" href="/static/favicon-32x32.png" />
       </Head>
       <body>
         <Main />
