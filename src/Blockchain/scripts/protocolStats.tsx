1f86e7f99e587946e5a35526ab01e2632c384126
Mehul0011 <mehul-intern@hashstack.finance>
2023-06-26 10:36:57 +0530

    New user stats functions

--- src/Blockchain/scripts/protocolStats.tsx
+++ src/Blockchain/scripts/protocolStats.tsx
@@ -1,5 +1,5 @@
 import { Contract, number, uint256 } from "starknet";
-import { getProvider, getTokenFromAddress, metricsContractAddress } from "../stark-constants";
+import { contractsEnv, getProvider, getTokenFromAddress, metricsContractAddress } from "../stark-constants";
 import metricsAbi from "../abis/metrics_abi.json";
 import { IMarketInfo, IProtocolReserves, NativeToken, Token } from "../interfaces/interfaces";
 import { parseAmount, weiToEtherNumber } from "../utils/utils";
@@ -18,13 +18,13 @@ function parseProtocolStats(market_infos: any): IMarketInfo[] {
         getTokenFromAddress(number.toHex(marketData?.token_address))?.name as Token
       ),
       lentAssets: weiToEtherNumber(
-       uint256.uint256ToBN(marketData?.lent_assets).toString(),
-       getTokenFromAddress(number.toHex(marketData?.token_address))?.name as Token
+        uint256.uint256ToBN(marketData?.lent_assets).toString(),
+        getTokenFromAddress(number.toHex(marketData?.token_address))?.name as Token
       ),
       totalBorrow: weiToEtherNumber(
         uint256.uint256ToBN(marketData?.total_borrow).toString(),
         getTokenFromAddress(number.toHex(marketData?.token_address))?.name as Token
-      ), 
+      ),
       availableReserves: weiToEtherNumber(
         uint256.uint256ToBN(marketData?.total_supply).sub(
           uint256.uint256ToBN(marketData?.total_borrow)
@@ -51,8 +51,8 @@ function parseProtocolStats(market_infos: any): IMarketInfo[] {
       ),
       exchangeRateUnderlyingToRtoken: parseAmount(
         uint256
-         .uint256ToBN(marketData?.exchange_rate_asset_to_rToken)
-         .toString(),
+          .uint256ToBN(marketData?.exchange_rate_asset_to_rToken)
+          .toString(),
         18
       ),
       exchangeRateUnderlyingToDtoken: parseAmount(
@@ -71,25 +71,111 @@ function parseProtocolStats(market_infos: any): IMarketInfo[] {
   return marketStats;
 }
 
-export async function getProtocolStats() {
-  const provider = getProvider();
-  const metricsContract = new Contract(
-    metricsAbi,
-    metricsContractAddress,
-    provider
-  );
-  try {
-    console.log("Get Protocol stats calling -----------");
-    const res = await metricsContract.call("get_protocol_stats", [], {
-      blockIdentifier: "pending",
-    });
-    console.log("Get protocol stats calling finished ---------");
-    return parseProtocolStats(res?.market_infos);
-  }
-  catch(error) {
-    console.error("getProtocolStats fails: ", error);
-  }
-}
+// function parseProtocolStat(marketData: any): IMarketInfo {
+//   let marketInfo: IMarketInfo = {
+//     borrowRate: parseAmount(uint256.uint256ToBN(marketData?.borrow_rate).toString(), 2),
+//     supplyRate: parseAmount(uint256.uint256ToBN(marketData?.supply_rate).toString(), 2),
+//     stakingRate: parseAmount(uint256.uint256ToBN(marketData?.staking_rate).toString(), 2),
+
+//     totalSupply: weiToEtherNumber(
+//       uint256.uint256ToBN(marketData?.total_supply).toString(),
+//       getTokenFromAddress(number.toHex(marketData?.token_address))?.name as Token
+//     ),
+//     lentAssets: weiToEtherNumber(
+//       uint256.uint256ToBN(marketData?.lent_assets).toString(),
+//       getTokenFromAddress(number.toHex(marketData?.token_address))?.name as Token
+//     ),
+//     totalBorrow: weiToEtherNumber(
+//       uint256.uint256ToBN(marketData?.total_borrow).toString(),
+//       getTokenFromAddress(number.toHex(marketData?.token_address))?.name as Token
+//     ),
+//     availableReserves: weiToEtherNumber(
+//       uint256.uint256ToBN(marketData?.total_supply).sub(
+//         uint256.uint256ToBN(marketData?.total_borrow)
+//       ).toString(),
+//       getTokenFromAddress(number.toHex(marketData?.token_address))?.name as Token
+//     ),
+//     utilisationPerMarket: parseAmount(uint256
+//       .uint256ToBN(marketData?.utilisation_per_market)
+//       .toString(),
+//       2
+//     ),
+
+//     exchangeRateRtokenToUnderlying: parseAmount(
+//       uint256
+//         .uint256ToBN(marketData?.exchange_rate_rToken_to_asset)
+//         .toString(),
+//       18
+//     ),
+//     exchangeRateDTokenToUnderlying: parseAmount(
+//       uint256
+//         .uint256ToBN(marketData?.exchange_rate_dToken_to_asset)
+//         .toString(),
+//       18
+//     ),
+//     exchangeRateUnderlyingToRtoken: parseAmount(
+//       uint256
+//         .uint256ToBN(marketData?.exchange_rate_asset_to_rToken)
+//         .toString(),
+//       18
+//     ),
+//     exchangeRateUnderlyingToDtoken: parseAmount(
+//       uint256
+//         .uint256ToBN(marketData?.exchange_rate_asset_to_dToken)
+//         .toString(),
+//       18
+//     ),
+
+//     tokenAddress: number.toHex(marketData?.token_address),
+//     token: getTokenFromAddress(number.toHex(marketData?.token_address))?.name as NativeToken,
+//   };
+//   return marketInfo;
+// }
+
+
+// export async function getProtocolStats() {
+//   const provider = getProvider();
+//   const metricsContract = new Contract(
+//     metricsAbi,
+//     metricsContractAddress,
+//     provider
+//   );
+//   try {
+//     console.log("Get Protocol stats calling -----------");
+//     const res = await metricsContract.call("get_protocol_stats", [], {
+//       blockIdentifier: "pending",
+//     });
+//     console.log("Get protocol stats calling finished ---------");
+//     return parseProtocolStats(res?.market_infos);
+//   }
+//   catch(error) {
+//     console.error("getProtocolStats fails: ", error);
+//   }
+// }
+
+// export async function getProtocolStats() {
+//   const marketStats: IMarketInfo[] = [];
+//   const provider = getProvider();
+//   const metricsContract = new Contract(
+//     metricsAbi,
+//     metricsContractAddress,
+//     provider
+//   );
+//   for (let i = 0; i < contractsEnv.TOKENS.length; ++i) {
+//     const token = contractsEnv.TOKENS[i];
+
+//     const res = await metricsContract.call("get_protocol_stats", [], {
+//       blockIdentifier: "pending",
+//     });
+//     const market_info = parseProtocolStat(res?.market_infos);
+//     // if (now.getTime() - lastUpdated.getTime() > MAX_ORACLE_LATENCY_MS) {
+//     //   Global.warning(`Oracle price for ${token.name} is too old: ${lastUpdated}`);
+//     // }
+//     marketStats.push(market_info);
+//   }
+//   return marketStats;
+// }
+
 
 function parseProtocolReserves(protocolReservesData: any): IProtocolReserves {
   let protocolReserves: IProtocolReserves = {
@@ -124,3 +210,6 @@ export async function getProtocolReserves() {
   });
   return parseProtocolReserves(res?.protocol_reserves);
 }
+
+
+
