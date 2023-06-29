4294473cc79d58c6d77628fd7b3780eb1e17e8ef
Sahitya77 <sahityanijhawan@gmail.com>
2023-06-29 12:49:14 +0530

    Your metrics and protocol metrics updated

--- src/components/layouts/charts/BorrowApr.tsx
+++ src/components/layouts/charts/BorrowApr.tsx
@@ -3,35 +3,22 @@ import dynamic from "next/dynamic";
 import { Box } from "@chakra-ui/react";
 import { ApexOptions } from "apexcharts";
 const ApexCharts = dynamic(() => import("react-apexcharts"), { ssr: false });
-const BorrowAprChart = () => {
+const BorrowAprChart = ({
+  series,
+  formatter,
+  color,
+  categories,
+  chartype
+}:any) => {
   const splineChartData = {
-    series: [
-      {
-        name: "Series 1",
-        data: [
-          30000, 40000, 35000, 50000, 49000, 60000, 70000, 91000, 12500, 98000,
-          110000,
+    series: series
+      ? series
+      : [
+          {
+            name: "Series 1",
+            data: [30000, 40000, 35000, 50000, 49000, 60000, 80000],
+          },
         ],
-        fill: {
-          colors: ["#005CC5"], // Specify the fill color for the area under the line
-          opacity: 0.5, // Set the opacity of the fill color (optional)
-        },
-        dataPoints: {
-          hidden: true, // Hide the data points in the area
-        },
-      },
-      // {
-      //   name: 'Series 2',
-      //   data: [23, 45, 67, 22, 56, 89, 33, 55, 79],
-      //   fill: {
-      //     colors: ['#00FF00'], // Specify the fill color for the area under the line
-      //     opacity: 0.5, // Set the opacity of the fill color (optional)
-      //   },
-      //   dataPoints: {
-      //       hidden: true, // Hide the data points in the area
-      //     },
-      // },
-    ],
     options: {
       chart: {
         toolbar: {
@@ -40,14 +27,19 @@ const BorrowAprChart = () => {
       },
       dataLabels: {
         enabled: false,
+        style:{
+          colors:["#000000"]
+        },
+        formatter: function(val:any) {
+          return val / 1000 + "k";; // Display the data value as the label
+        },
+        position: 'top',
       },
+      markers: {
+        size: 2,
+        colors:["#fff"]
+    },
       xaxis: {
-        tooltip: {
-          enabled: false, // Disable the x-axis tooltip
-        },
-        axisTicks: {
-          show: false, // Hide the small spikes at x-axis labels
-        },
         labels: {
           style: {
             colors: "#6E7681", // Set the color of the labels
@@ -55,134 +47,63 @@ const BorrowAprChart = () => {
             fontWeight: "400",
           },
         },
-        axisBorder: {
-          color: "grey", // Set the color of the x-axis lines
+        axisTicks: {
+          show: false,
         },
-        categories: [
-          "Jan",
-          "Feb",
-          "Mar",
-          "Apr",
-          "May",
-          "Jun",
-          "Jul",
-          "Aug",
-          "Sep",
-          "Nov",
-          "Dec",
-        ],
+        axisBorder: {
+          color: "grey",
+        },
+        categories: categories ? categories : [1, 2, 3, 4, 5, 6, 7],
       },
       yaxis: {
         labels: {
-          formatter: function (value: any) {
-            return value / 1000 + "k"; // Divide by 1000 and append 'k' for thousands
-          },
+          formatter: formatter
+            ? formatter
+            : function (value: any) {
+                return value / 1000 + "k";
+              },
           style: {
             colors: "#6E7681", // Set the color of the labels
             fontSize: "12px",
             fontWeight: "400",
           },
+        },
+        min: 0,
+      },
+      plotOptions: {
+        bar: {
+          opacity: 1, // Set the opacity to 1 for fully opaque bars
+          columnWidth: "70%", // Adjust the column width for better spacing between bars
+          colors: {
+            backgroundBarOpacity: 1, // Set the opacity of the background bar
+          },
+        },
+      },
+      colors: [`${color ? color : "#0FCA7A"}`],
+      grid: {
+        borderColor: "#2B2F35",
+        padding: {
+          bottom: 10, // Add bottom padding to prevent overlap with x-axis labels
         },
       },
       annotations: {
         xaxis: [
           {
-            x: "Jan", // Specify the x-axis value where the line should appear
-            strokeDashArray: 0, // Set the length of the dash for the line
-            borderColor: "#2B2F35", // Set the color of the line
-            borderWidth: 1, // Set the width of the line
-          },
-          {
-            x: "Feb", // Specify the x-axis value where the line should appear
-            strokeDashArray: 0, // Set the length of the dash for the line
-            borderColor: "#2B2F35", // Set the color of the line
-            borderWidth: 1, // Set the width of the line
-          },
-          {
-            x: "Mar", // Specify the x-axis value where the line should appear
-            strokeDashArray: 0, // Set the length of the dash for the line
-            borderColor: "#2B2F35", // Set the color of the line
-            borderWidth: 1, // Set the width of the line
-          },
-          {
-            x: "Apr", // Specify the x-axis value where the line should appear
-            strokeDashArray: 0, // Set the length of the dash for the line
-            borderColor: "#2B2F35", // Set the color of the line
-            borderWidth: 1, // Set the width of the line
-          },
-          {
-            x: "May", // Specify the x-axis value where the line should appear
-            strokeDashArray: 0, // Set the length of the dash for the line
-            borderColor: "#2B2F35", // Set the color of the line
-            borderWidth: 1, // Set the width of the line
-          },
-          {
-            x: "Jun", // Specify the x-axis value where the line should appear
-            strokeDashArray: 0, // Set the length of the dash for the line
-            borderColor: "#2B2F35", // Set the color of the line
-            borderWidth: 1, // Set the width of the line
-          },
-          {
-            x: "Jul", // Specify the x-axis value where the line should appear
-            strokeDashArray: 0, // Set the length of the dash for the line
-            borderColor: "#2B2F35", // Set the color of the line
-            borderWidth: 1, // Set the width of the line
-          },
-          {
-            x: "Aug", // Specify the x-axis value where the line should appear
-            strokeDashArray: 0, // Set the length of the dash for the line
-            borderColor: "#2B2F35", // Set the color of the line
-            borderWidth: 1, // Set the width of the line
-          },
-          {
-            x: "Sep", // Specify the x-axis value where the line should appear
-            strokeDashArray: 0, // Set the length of the dash for the line
-            borderColor: "#2B2F35", // Set the color of the line
-            borderWidth: 1, // Set the width of the line
-          },
-          {
-            x: "Oct", // Specify the x-axis value where the line should appear
-            strokeDashArray: 0, // Set the length of the dash for the line
-            borderColor: "#2B2F35", // Set the color of the line
-            borderWidth: 1, // Set the width of the line
-          },
-          {
-            x: "Nov", // Specify the x-axis value where the line should appear
-            strokeDashArray: 0, // Set the length of the dash for the line
-            borderColor: "#2B2F35", // Set the color of the line
-            borderWidth: 1, // Set the width of the line
-          },
-          {
-            x: "Dec", // Specify the x-axis value where the line should appear
-            strokeDashArray: 0, // Set the length of the dash for the line
-            borderColor: "#2B2F35", // Set the color of the line
-            borderWidth: 1, // Set the width of the line
+            x: 0,
+            strokeDashArray: 0,
+            borderColor: "grey",
+            borderWidth: 1,
           },
         ],
-      },
-      stroke: {
-        curve: "smooth",
-        colors: ["#005CC5"],
-        opacity: "100%",
-      },
-      grid: {
-        borderColor: "#2B2F35",
       },
     },
   };
 
-  const options: ApexOptions = {
-    ...splineChartData.options,
-    stroke: {
-      ...splineChartData.options.stroke,
-      curve: "smooth",
-    },
-  };
 
   return (
     <Box border="1px solid #2B2F35" borderRadius="6px" padding="16px 24px 40px">
       <ApexCharts
-        options={options}
+        options={splineChartData.options}
         series={splineChartData.series}
         type="line"
         height={350}
--- src/components/layouts/metrics/borrowMetrics.tsx
+++ src/components/layouts/metrics/borrowMetrics.tsx
@@ -6,6 +6,7 @@ import SupplyAprChart from "../charts/SupplyApr";
 import BorrowAprChart from "../charts/BorrowApr";
 // import { Button } from "reactstrap";
 import SupplyAPRLiquidityProvider from "../charts/supplyAPRLiquitityProvider";
+import YourMetricsBorrow from "../charts/YourMetricsBorrow";
 const BorrowMetrics = ({ currentMarketCoin }: any) => {
   const series1: any = [
     [
@@ -169,7 +170,7 @@ const BorrowMetrics = ({ currentMarketCoin }: any) => {
           >
             Borrow APR:{" "}
           </Box>
-          <AssetUtilizationChart color={"#2BA26F"} />
+          <BorrowAprChart color={"#2BA26F"} />
         </Box>
       </Box>
       <Box display="flex" gap="30px">
@@ -279,6 +280,30 @@ const BorrowMetrics = ({ currentMarketCoin }: any) => {
           <AssetUtilizationChart
             color={"#2BA26F"}
             series={series2[borrowerChartPeriod]}
+          />
+        </Box>
+      </Box>
+      <Box display="flex" gap="30px">
+        <Box display="flex" flexDirection="column" gap="8px" width="50%">
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="flex-start"
+            height="72px"
+            border="1px solid #2B2F35"
+            color="#E6EDF3"
+            padding="24px 24px 16px"
+            fontSize="20px"
+            fontStyle="normal"
+            fontWeight="600"
+            lineHeight="30px"
+            borderRadius="6px"
+          >
+            Median Borrow By Market:{" "}
+          </Box>
+          <YourMetricsBorrow
+            color={"#846ED4"}
+            // series={series2[liquidityProviderChartPeriod]}
           />
         </Box>
       </Box>
