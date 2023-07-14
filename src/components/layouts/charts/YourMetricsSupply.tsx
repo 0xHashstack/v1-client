57f17753e9348f05f880830c7c63e70ac69a7434
aryan <aryan6396596910negi@gmail.com>
2023-07-14 12:34:16 +0530

    your metrics supply chart data fixed

--- src/components/layouts/charts/YourMetricsSupply.tsx
+++ src/components/layouts/charts/YourMetricsSupply.tsx
@@ -2,35 +2,48 @@ import React, { useEffect, useState } from "react";
 import { Box } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
 import { useSelector } from "react-redux";
-import { selectUserDeposits } from "@/store/slices/readDataSlice";
+import {
+  selectOraclePrices,
+  selectUserDeposits,
+} from "@/store/slices/readDataSlice";
 import { IDeposit } from "@/Blockchain/interfaces/interfaces";
 import numberFormatter from "@/utils/functions/numberFormatter";
 const ApexCharts = dynamic(() => import("react-apexcharts"), { ssr: false });
 const YourMetricsSupply = ({ series, formatter, color, categories }: any) => {
   const [supplyData, setSupplyData] = useState(null);
   const userDeposits = useSelector(selectUserDeposits);
+  const oraclePrices = useSelector(selectOraclePrices);
+
   useEffect(() => {
     try {
       const fetchSupplyData = async () => {
         const data = userDeposits?.map((deposit: IDeposit, idx: number) => {
-          return (
-            deposit?.rTokenFreeParsed +
-            deposit?.rTokenLockedParsed +
-            deposit?.rTokenStakedParsed
-          );
+          const price = oraclePrices?.find(
+            (oraclePrice: any) => oraclePrice?.name == deposit?.token
+          )?.price;
+          const token_amount = deposit?.underlyingAssetAmountParsed;
+          if (price && token_amount) {
+            return price * token_amount;
+          }
+          return 0;
         });
         if (data && data?.length > 0) {
           setSupplyData(data);
         }
         console.log("supplyData", data);
       };
-      if (userDeposits && userDeposits?.length > 0) {
+      if (
+        userDeposits &&
+        userDeposits?.length > 0 &&
+        oraclePrices &&
+        oraclePrices?.length > 0
+      ) {
         fetchSupplyData();
       }
     } catch (err) {
       console.log("your metrics supply err ", err);
     }
-  }, [userDeposits]);
+  }, [userDeposits, oraclePrices]);
 
   const chartOptions = {
     chart: {
@@ -50,20 +63,22 @@ const YourMetricsSupply = ({ series, formatter, color, categories }: any) => {
       position: "top",
     },
     xaxis: {
-        labels: {
-            style: {
-              colors: "#6E7681", // Set the color of the labels
-              fontSize: "12px",
-              fontWeight: "400",
-            },
-          },
-          axisTicks: {
-            show: false,
-          },
-          axisBorder: {
-            color: "grey",
-          },
-          categories: categories ? categories : ["BTC","ETH","USDT","USDC","DAI"],
+      labels: {
+        style: {
+          colors: "#6E7681", // Set the color of the labels
+          fontSize: "12px",
+          fontWeight: "400",
+        },
+      },
+      axisTicks: {
+        show: false,
+      },
+      axisBorder: {
+        color: "grey",
+      },
+      categories: categories
+        ? categories
+        : ["BTC", "ETH", "USDT", "USDC", "DAI"],
     },
     plotOptions: {
       bar: {
@@ -81,7 +96,7 @@ const YourMetricsSupply = ({ series, formatter, color, categories }: any) => {
         formatter: formatter
           ? formatter
           : function (value: any) {
-              return numberFormatter(value);
+              return "$" + numberFormatter(value);
             },
         style: {
           colors: "#6E7681", // Set the color of the labels
@@ -123,7 +138,6 @@ const YourMetricsSupply = ({ series, formatter, color, categories }: any) => {
       data: supplyData ? supplyData : [44000, 55000, 41000, 17000, 15000],
     },
   ];
-  
 
   return (
     <Box border="1px solid #2B2F35" borderRadius="6px" padding="16px 24px 40px">
