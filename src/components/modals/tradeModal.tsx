c7796ea06abc9c87e8bf65b89a20220e4615b244
Sahitya77 <sahityanijhawan@gmail.com>
2023-09-14 17:08:56 +0530

    BUG_FIX Updated borrow market wrong token fix [DONE]

--- src/components/modals/tradeModal.tsx
+++ src/components/modals/tradeModal.tsx
@@ -431,7 +431,10 @@ const TradeModal = ({
     // console.log("radio value", radioValue, method);
   }, [radioValue]);
   const [tokenTypeSelected, setTokenTypeSelected] = useState("Native");
-
+  useEffect(()=>{
+    setLoanMarket(coin ? coin.name:"BTC")
+    setCollateralMarket(coin ? coin.name:"BTC")
+  },[coin])
   const resetStates = () => {
     setSliderValue(0);
     setsliderValue2(0);
@@ -442,10 +445,10 @@ const TradeModal = ({
     setLoanAmount(0);
     setCurrentDapp("Select a dapp");
     setCurrentPool("Select a pool");
-    setCurrentCollateralCoin(coin?.name);
-    setCollateralMarket(coin?.name);
-    setCurrentBorrowCoin(coin?.name);
-    setLoanMarket(coin?.name);
+    setCurrentCollateralCoin(coin ? coin.name:"BTC");
+    setCollateralMarket(coin ? coin.name:"BTC");
+    setCurrentBorrowCoin(coin ? coin.name:"BTC");
+    setLoanMarket(coin ? coin.name:"BTC");
     setCurrentPoolCoin("Select a pool");
     setRadioValue("1");
     setHealthFactor(undefined);
