16eaffe5b25475e2aa33f9f1ff733860b0a0b6c6
Arun89-crypto <arunjngra89@gmail.com>
2023-09-22 20:38:50 +0530

    feat: added api updates && deploy script

--- src/pages/api/feedback/rating.tsx
+++ src/pages/api/feedback/rating.tsx
@@ -1,57 +1,62 @@
-import { google } from 'googleapis';
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { google } from "googleapis";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    console.log("========================");
+    console.log(req);
+    console.log(req.headers);
+    console.log(req.body);
+    console.log("========================");
+
     const { starRating, address } = req.body;
-    const Datetime=new Date();
-    try{
+    const Datetime = new Date();
+    try {
       if (starRating === undefined) {
         // If starRating is not provided in the request body, send an error response.
-        res.status(400).json({ error: 'starRating is required' });
+        res.status(400).json({ error: "starRating is required" });
       } else {
-       const auth=new google.auth.GoogleAuth({
-        credentials:{
-          client_email:process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL,
-          private_key:process.env.NEXT_PUBLIC_GOOGLE_PRIVATE_KEY?.replace(/\\n/g,"\n")
-        },
-        scopes:[
-          'https://www.googleapis.com/auth/drive',
-          'https://www.googleapis.com/auth/drive.file',
-          'https://www.googleapis.com/auth/spreadsheets'
-        ]
-       })
-       const sheets=google.sheets({
-        auth,
-        version:'v4'
-       })
+        const auth = new google.auth.GoogleAuth({
+          credentials: {
+            client_email: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL,
+            private_key: process.env.NEXT_PUBLIC_GOOGLE_PRIVATE_KEY?.replace(
+              /\\n/g,
+              "\n"
+            ),
+          },
+          scopes: [
+            "https://www.googleapis.com/auth/drive",
+            "https://www.googleapis.com/auth/drive.file",
+            "https://www.googleapis.com/auth/spreadsheets",
+          ],
+        });
+        const sheets = google.sheets({
+          auth,
+          version: "v4",
+        });
 
-       const response=await sheets.spreadsheets.values.append({
-          spreadsheetId:process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID,
-          range:'A1:C1',
-          valueInputOption:'USER_ENTERED',
-          requestBody:{
-            values:[
-              [
-                Datetime,address,starRating
-              ]
-            ]
-          }
-       })
-        
+        const response = await sheets.spreadsheets.values.append({
+          spreadsheetId: process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID,
+          range: "A1:C1",
+          valueInputOption: "USER_ENTERED",
+          requestBody: {
+            values: [[Datetime, address, starRating]],
+          },
+        });
+
         // Here, you can handle the rating data and save it to your backend or database.
         // Example: Save the rating to a database.
-        res.status(200).json(
-          { 
-            message: 'Rating submitted successfully',
-            rating:starRating,
-            data:response.data
-          }
-          );
+        res.status(200).json({
+          message: "Rating submitted successfully",
+          rating: starRating,
+          data: response.data,
+        });
       }
-    } 
-    catch(e:any){
-      return res.status(400).send({message:e.message})
+    } catch (e: any) {
+      return res.status(400).send({ message: e.message });
     }
   }
 }
