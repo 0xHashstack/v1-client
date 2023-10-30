1f97fccb936fe48fc24e70a9273124e6cea6dba3
Sahitya77 <sahityanijhawan@gmail.com>
2023-10-30 17:09:02 +0530

    REFACTORING Updated code structure and navbar [IN_PORGRESS]

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,9 +1,30 @@
+import Navbar from '@/components/Navbar'
 import { Box } from '@chakra-ui/react'
 import React from 'react'
 const form = () => {
   return (
     <Box>
-      Form
+      <Box background="rgba(103, 109, 154, 0.10)"
+        position={'fixed'} zIndex={3}
+      >
+        <Navbar/>
+      </Box>
+      <Box
+        background={`
+                radial-gradient(circle 1800px at top left, rgba(115, 49, 234, 0.15), transparent) top left,
+                radial-gradient(circle 1300px at bottom right, rgba(115, 49, 234, 0.15), transparent) bottom right,
+                black
+              `}
+        color="white"
+        zIndex={1}
+        padding="0"
+
+        alignItems="center"
+        minHeight={"100vh"}
+        pt="8rem"
+      >
+        Form
+      </Box>
     </Box>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import HashstackLogo from '@/assets/hashstacklogo'
+import { HStack,Text } from '@chakra-ui/react'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <HStack
+    padding="10px"
+    width="50vw"
+    display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      color="#fff"
+      height="3.8125rem"
+    >
+        <HashstackLogo/>
+        <HStack color="white">
+            <Text color="white">
+                Connect Wallet
+            </Text>
+            <Text color="white">
+                Settings
+            </Text>
+        </HStack>
+    </HStack>
+  )
+}
+
+export default Navbar
