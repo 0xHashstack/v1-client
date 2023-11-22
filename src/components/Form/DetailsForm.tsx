5f1a93834e33f813d750be61f0e3a6d7581ce075
Sahitya77 <sahityanijhawan@gmail.com>
2023-11-22 15:10:45 +0530

    REFACTORING Updated content for HASH Tokens [IN_PROGRESS]

--- src/components/Form/DetailsForm.tsx
+++ src/components/Form/DetailsForm.tsx
@@ -177,6 +177,17 @@ const DetailsForm = ({ handler }: any) => {
             <Input
             color="white"
               border="0px"
+              placeholder="Aprillyto#7879"
+              _placeholder={{
+                color: "rgba(240, 240, 245, 0.50)",
+                fontFamily: "Inter",
+                fontSize: "14px",
+                fontStyle: "normal",
+                fontWeight: "500",
+                lineHeight: "20px",
+                letterSpacing: "-0.15px"
+
+              }}
               value={discord}
               onChange={handleDiscordChange}
             ></Input>
@@ -690,7 +701,25 @@ const DetailsForm = ({ handler }: any) => {
           borderRadius={"8px"}
           border="1px solid var(--stroke-of-30, rgba(103, 109, 154, 0.30));"
           background=" var(--surface-of-10, rgba(103, 109, 154, 0.10));"
-        ></Box>
+          padding="33px 42px"
+          font-size=" 18px"
+          fontWeight="400"
+          lineHeight="30px"
+          letterSpacing="-0.15px"
+        >
+          <Text>
+            Hash Tokens
+          </Text>
+          <Text mt="2rem">
+          The HASH token is a key catalyst in the Hashstack ecosystem, and will serve 3 primary objectives. <br/>
+          1. Store of authority(Governance): To enable decentralised governance. <br/>
+          2. Store of value(Utility): For payment of in-dapp transaction fees, compensating partner projects, KOLs, and community participants who help secure/further the Hashstack ecosystem. <br/>
+          3. Unlock liquidator role: Liquidators on Hashstack take the responsibility of repaying the bad debt to the Hashstack protocol, in-exchange for acquiring them at a discount. 
+          </Text>
+          <Text mt="3rem">
+          HASH tokens total supply is hard capped to 9,000,000,000 (9 billion). For TGE, Hashstack has partnered with the Industry leading launchpad - Tokensoft that helped launch Ava (Avalanche token), GRAPH (The Graph token) among other notable projects. Leave a place holder to fit 4 bullet points (2 lines each) for Disclaimers
+          </Text>
+        </Box>
       </HStack>
     </>
   );
--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -18,15 +18,15 @@ const form = () => {
     token:"0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
   })
   const [loading, setLoading] = useState<Boolean>(false);
-  useEffect(()=>{
-    if(address && (Number(usdtBalance?.data?.formatted) > 50 || Number(usdcBalance?.data?.formatted) > 50)){
-      setLoading(false);
-    }
+  // useEffect(()=>{
+  //   if(address && (Number(usdtBalance?.data?.formatted) > 50 || Number(usdcBalance?.data?.formatted) > 50)){
+  //     setLoading(false);
+  //   }
 
-    else{
-      router.push('/');
-    }
-  },[address])
+  //   else{
+  //     router.push('/');
+  //   }
+  // },[address])
 
   return (
  <Box>
@@ -56,7 +56,7 @@ const form = () => {
         pt="8rem"
       >
         <Text    color="white" mb="4rem">
-          Presale form
+          Presale Interest form
         </Text>
         {/* <ContributorsChart/> */}
         <HStack
@@ -68,8 +68,6 @@ const form = () => {
     >
         <DetailsForm/>
         {/* <DetailsForm/> */}
-
-
       </HStack>
    
         <Text color="white" mt="3rem" mb="2rem">
