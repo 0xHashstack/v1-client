bdac51a856f1bdf043da64e9753dfa65db3dd0c0
Hemant <hrsmart2150@gmail.com>
2024-04-16 18:29:29 +0530

    Refactored docs of the all the storybook components

--- src/stories/Banners/index.stories.ts
+++ src/stories/Banners/index.stories.ts
@@ -1,6 +1,6 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
-import {Banners} from './index'
+import type { Meta, StoryObj } from '@storybook/react'
+import { fn } from '@storybook/test'
+import { Banners } from './index'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
@@ -14,19 +14,51 @@ const meta = {
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    variants: {
+      control: {
+        type: 'object',
+        options: {
+          info: {
+            label: 'Info',
+            name: 'info',
+          },
+          warning: {
+            label: 'Warning',
+            name: 'warning',
+          },
+          danger: {
+            label: 'Danger',
+            name: 'danger',
+          },
+        },
+      },
+    },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
-} satisfies Meta<typeof Banners>;
+} satisfies Meta<typeof Banners>
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+export default meta
+type Story = StoryObj<typeof meta>
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: 'CheckBox',
+    variants: {
+      info: {
+        label:
+          'You have selected a native token as collateral which will be converted to rtokens 1rUSDC = 1.0255USDC',
+        name: 'info',
+      },
+      warning: {
+        label:
+          "The current collateral and borrowing market combination isn't allowed at this moment.",
+        name: 'warning',
+      },
+      danger: {
+        label:
+          'We are evaluating few promising DApps to integrate. Please check back at a late time.',
+        name: 'danger',
+      },
+    },
   },
-};
+}
--- src/stories/Banners/index.tsx
+++ src/stories/Banners/index.tsx
@@ -1,199 +1,156 @@
-import AlertTrade from '@/assets/icons/alertTrade'
-import ArrowUp from '@/assets/icons/arrowup'
+import { Box, Text } from '@chakra-ui/react'
+
 import BlueInfoIcon from '@/assets/icons/blueinfoicon'
-import BTCLogo from '@/assets/icons/coins/btc'
-import DAILogo from '@/assets/icons/coins/dai'
-import ETHLogo from '@/assets/icons/coins/eth'
-import STRKLogo from '@/assets/icons/coins/strk'
-import USDCLogo from '@/assets/icons/coins/usdc'
-import USDTLogo from '@/assets/icons/coins/usdt'
-import DropdownUp from '@/assets/icons/dropdownUpIcon'
 import RedinfoIcon from '@/assets/icons/redinfoicon'
-import TableClose from '@/components/layouts/table/tableIcons/close'
-import { Box, Checkbox as Ch, Text } from '@chakra-ui/react'
-import React, { useState } from 'react'
-import Image from 'next/image'
 import TableInfoIcon from '@/components/layouts/table/tableIcons/infoIcon'
+
 interface CheckBoxProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean
-
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string
-
-  variants?: {
-    INFO: 'info'
-    DANGER: 'danger'
-    SUCCESS: 'success'
-    WARNING: 'warning'
+  variants: {
+    info: {
+      label: string
+      name: string
+    }
+    warning: {
+      label: string
+      name: string
+    }
+    danger: {
+      label: string
+      name: string
+    }
   }
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large'
-  /**
-   * Button contents
-   */
-  label: string
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void
 }
 
 /**
  * Primary UI component for user interaction
  */
-export const Banners = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: CheckBoxProps) => {
-  const getCoin = (CoinName: string) => {
-    switch (CoinName) {
-      case 'BTC':
-        return <BTCLogo height={'28px'} width={'28px'} />
-      case 'USDC':
-        return <USDCLogo height={'16px'} width={'16px'} />
-      case 'USDT':
-        return <USDTLogo height={'16px'} width={'16px'} />
-      case 'ETH':
-        return <ETHLogo height={'16px'} width={'16px'} />
-      case 'DAI':
-        return <DAILogo height={'16px'} width={'16px'} />
-      case 'STRK':
-        return <STRKLogo height={'16px'} width={'16px'} />
-      default:
-        break
-    }
-  }
-
+export const Banners = ({ variants, ...props }: CheckBoxProps) => {
   return (
-    <Box display="flex" flexDirection="column" gap="2rem">
-      <Box display="flex" flexDirection="column" gap=".3rem">
-        <Text color="white" fontSize="sm">
-          Info
-        </Text>
+    <Box display="flex" flexDirection="column" gap="2rem" {...props}>
+      {variants.info.name === 'info' && (
+        <Box display="flex" flexDirection="column" gap=".3rem">
+          <Text color="white" fontSize="sm">
+            Info
+          </Text>
 
-        <Box
-          display="flex"
-          bg={'#222766'}
-          color="#F0F0F5"
-          fontSize="12px"
-          p="4"
-          border={'1px solid #3841AA'}
-          fontStyle="normal"
-          fontWeight="400"
-          lineHeight="18px"
-          borderRadius="6px"
-        >
-          <Box pr="3" mt="0.6" cursor="pointer">
-            <BlueInfoIcon />
-          </Box>
-          You have selected a native token as collateral which will be converted
-          to rtokens 1rUSDC = 1.0255USDC
-        </Box>
-      </Box>
-
-      <Box
-        display="flex"
-        flexDirection="column"
-        justifyContent="left"
-        w="100%"
-        pb="2"
-        pt="4"
-        gap=".3rem"
-      >
-        <Text color="white" fontSize="sm">
-          Info 2.0
-        </Text>
-        <Box
-          width="full"
-          display="flex"
-          bg="#676D9A4D"
-          fontSize="12px"
-          p="4"
-          fontStyle="normal"
-          fontWeight="400"
-          borderRadius="6px"
-          border="1px solid #3841AA"
-          color="#F0F0F5"
-          gap=".7rem"
-        >
-          <Box mt="3px">
-            <TableInfoIcon />
-          </Box>
-          The staked rTokens cannot be used as collateral until unstaked.
-        </Box>
-      </Box>
-
-      <Box display="flex" flexDirection="column" gap=".3rem">
-        <Text color="white" fontSize="sm">
-          Error
-        </Text>
-
-        <Box
-          display="flex"
-          bg={'#480C10'}
-          color="#F0F0F5"
-          fontSize="12px"
-          p="4"
-          border={'1px solid #9B1A23'}
-          fontStyle="normal"
-          fontWeight="400"
-          lineHeight="18px"
-          borderRadius="6px"
-        >
-          <Box pr="3" mt="0.9" cursor="pointer">
-            <RedinfoIcon />
-          </Box>
-          The current collateral and borrowing market combination isn&apos;t
-          allowed at this moment.
-          <Box></Box>
-        </Box>
-      </Box>
-
-      <Box display="flex" flexDirection="column" gap=".3rem">
-        <Text color="white" fontSize="sm">
-          Warning
-        </Text>
-
-        <Box
-          display="flex"
-          border="1px solid #A48007"
-          bg="#A480074D"
-          fontSize="14px"
-          p="8px"
-          fontStyle="normal"
-          fontWeight="400"
-          borderRadius="6px"
-          justifyContent="start"
-          alignItems="flex-start"
-        >
           <Box
-            cursor="pointer"
             display="flex"
-            justifyContent="flex-start"
-            alignItems="flex-start"
-            pt="2px"
-            pr="4px"
+            bg={'#222766'}
+            color="#F0F0F5"
+            fontSize="12px"
+            p="4"
+            border={'1px solid #3841AA'}
+            fontStyle="normal"
+            fontWeight="400"
+            lineHeight="18px"
+            borderRadius="6px"
           >
-            <AlertTrade />
-          </Box>
-          <Box p="6px 2px" display="flex">
-            <Text mt="0.9" fontSize="sm" color="#F0F0F5">
-              We are evaluating few promising DApps to integrate. Please check
-              back at a late time.
-            </Text>
+            <Box pr="3" mt="0.6" cursor="pointer">
+              <BlueInfoIcon />
+            </Box>
+            {variants.info.label}
           </Box>
         </Box>
-      </Box>
+      )}
+
+      {variants.info.name === 'info' && (
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="left"
+          w="100%"
+          pb="2"
+          pt="4"
+          gap=".3rem"
+        >
+          <Text color="white" fontSize="sm">
+            Info 2.0
+          </Text>
+          <Box
+            width="full"
+            display="flex"
+            bg="#676D9A4D"
+            fontSize="12px"
+            p="4"
+            fontStyle="normal"
+            fontWeight="400"
+            borderRadius="6px"
+            border="1px solid #3841AA"
+            color="#F0F0F5"
+            gap=".7rem"
+          >
+            <Box mt="3px">
+              <TableInfoIcon />
+            </Box>
+            {variants.info.label}
+          </Box>
+        </Box>
+      )}
+
+      {variants.danger.name === 'danger' && (
+        <Box display="flex" flexDirection="column" gap=".3rem">
+          <Text color="white" fontSize="sm">
+            Error
+          </Text>
+
+          <Box
+            display="flex"
+            bg={'#480C10'}
+            color="#F0F0F5"
+            fontSize="12px"
+            p="4"
+            border={'1px solid #9B1A23'}
+            fontStyle="normal"
+            fontWeight="400"
+            lineHeight="18px"
+            borderRadius="6px"
+          >
+            <Box pr="3" mt="0.9" cursor="pointer">
+              <RedinfoIcon />
+            </Box>
+            {variants.danger.label}
+          </Box>
+        </Box>
+      )}
+
+      {variants.warning.name === 'warning' && (
+        <Box display="flex" flexDirection="column" gap=".3rem">
+          <Text color="white" fontSize="sm">
+            Warning
+          </Text>
+
+          <Box
+            display="flex"
+            border="1px solid #A48007"
+            bg="#A480074D"
+            fontSize="14px"
+            p="8px"
+            fontStyle="normal"
+            fontWeight="400"
+            borderRadius="6px"
+            justifyContent="start"
+            alignItems="flex-start"
+          >
+            <Box
+              display="flex"
+              bg={'#222766'}
+              color="#F0F0F5"
+              fontSize="12px"
+              p="4"
+              border={'1px solid #3841AA'}
+              fontStyle="normal"
+              fontWeight="400"
+              lineHeight="18px"
+              borderRadius="6px"
+            >
+              <Box pr="3" mt="0.6" cursor="pointer">
+                <BlueInfoIcon />
+              </Box>
+              {variants.warning.label}
+            </Box>
+          </Box>
+        </Box>
+      )}
     </Box>
   )
 }
