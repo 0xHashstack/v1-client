07cb5ec5999cde712389b78468ea2eec4db7d95a
Sahitya77 <sahityanijhawan@gmail.com>
2024-08-29 11:56:22 +0530

    BUG_FIX Fixed errors for not loading and pushed amplify yaml file [DONE]

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,27 +4,6 @@ import { cookieToInitialState } from "wagmi";
 import { GetServerSideProps } from "next";
 import { config } from "@/services/wagmi/config";
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  let initialState: any = cookieToInitialState(config, context.req.headers.cookie);
-
-  if (initialState) {
-    // Handle connections Map serialization
-    if (initialState.connections instanceof Map) {
-      initialState.connections = Array.from(initialState.connections.entries());
-    }
-
-    // Replace undefined values with null
-    initialState = JSON.parse(JSON.stringify(initialState, (key, value) =>
-      value === undefined ? null : value
-    ));
-  }
-
-  return {
-    props: {
-      initialState: initialState || {},
-    },
-  };
-};
 
 export default function Home() {
   const router = useRouter();
