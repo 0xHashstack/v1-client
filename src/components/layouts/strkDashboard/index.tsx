39fa19bfe6429f1f3c8b0526bcbe7f894e6c8c5e
Sahitya77 <sahityanijhawan@gmail.com>
2024-09-09 15:36:26 +0530

    ENHANCMENET updated code for round 16 allocation [DONE]

--- src/components/layouts/strkDashboard/index.tsx
+++ src/components/layouts/strkDashboard/index.tsx
@@ -58,8 +58,8 @@ import CopyToClipboard from 'react-copy-to-clipboard'
 import { useDispatch, useSelector } from 'react-redux'
 import { toast } from 'react-toastify'
 import PageCard from '../pageCard'
-import dataStrkRewards from '../strkDashboard/round_15.json'
-import dataStrkRewardsZklend from '../strkDashboard/zkLend_15.json'
+import dataStrkRewards from '../strkDashboard/round_16.json'
+import dataStrkRewardsZklend from '../strkDashboard/zkLend_16.json'
 import { processAddress } from '@/Blockchain/stark-constants'
 export interface ICoin {
   name: string
